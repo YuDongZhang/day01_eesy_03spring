6cb3866dc04e8bb4a82a7a3339eb7f49b8fb4828
YuDongZhang <977872239@qq.com>
2021-07-01 17:17:25 +0800

    1,spring首次测试完成

---
+++ src/main/java/com/itheima/service/impl/AccountServiceImpl.java
@@ -0,0 +1,19 @@
+package com.itheima.service.impl;
+
+import com.itheima.dao.IAccountDao;
+import com.itheima.service.IAccountService;
+
+/**
+ * 账户的业务层实现类
+ */
+public class AccountServiceImpl implements IAccountService {
+
+
+
+    public void  saveAccount(){
+        int i = 1;
+
+        System.out.println(i);
+        i++;
+    }
+}
